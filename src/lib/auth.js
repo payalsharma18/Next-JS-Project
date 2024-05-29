bca992a88d27eb99651a545cacfc5a83a141fcef
payalsharma18 <payalsharma5394@gmail.com>
2024-05-29 17:11:43 +0530

    auth

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,40 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github"
+import GitHub from "next-auth/providers/github";
+import { connectToDb } from "./utils";
+import { User } from "./models";
+
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [GitHub({ clienId: process.env.GITHUB_ID, clientsecret: process.env.GITHUB_SECRET })],
-})
+    providers:
+        [GitHub({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+        ],
+    callbacks: {
+        async signIn({ user, account, profile }) {
+            if (account.provider === "github") {
+              connectToDb();
+              try {
+                const user = await User.findOne({ email: profile.email });
+      console.log(user);
+                if (!user) {
+                  const newUser = new User({
+                    username: profile.login,
+                    email: profile.email,
+                    image: profile.avatar_url,
+                  });
+      
+                  await newUser.save();
+                }
+              } catch (err) {
+                console.log(err);
+                return false;
+              }
+            }
+            return true;
+          },
+            
+        
+    }
+});
